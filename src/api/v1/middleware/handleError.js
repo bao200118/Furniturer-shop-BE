6583463b8bcfef0b34160920d94a232bae8d184a
bao <bao200118@gmail.com>
2022-03-31 23:33:58 +0700

    fix error handler

--- src/api/v1/middleware/handleError.js
+++ src/api/v1/middleware/handleError.js
@@ -1,5 +1,8 @@
 function handleError(err, req, res, next) {
-    return res.status(err.status || 500).send(error.message);
+    return res.status(err.status || 500).send({
+        status: err.status,
+        message: err.message,
+    });
 }
 
 module.exports = handleError;
