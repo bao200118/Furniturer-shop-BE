db693624bd5f2a47c1a45ef7fd6ba61f5e229521
bao200118 <60221952+bao200118@users.noreply.github.com>
2022-12-07 01:32:55 +0000

    Fix bcrypt package

--- src/api/v1/controllers/UserController.js
+++ src/api/v1/controllers/UserController.js
@@ -1,71 +1,68 @@
-const { json } = require('express/lib/response');
-const bcrypt = require('bcrypt');
-const userModel = require('../models/userModel');
-const { CustomError } = require('../util/CustomError');
+const { json } = require("express/lib/response");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/userModel");
+const { CustomError } = require("../util/CustomError");
 
 class UserController {
-    updateUser = async (req, res, next) => {
-        try {
-            await userModel.findByIdAndUpdate(req.user._id, {
-                name: req.body.name,
-                gender: req.body.gender,
-                phone: req.body.phone,
-            });
+	updateUser = async (req, res, next) => {
+		try {
+			await userModel.findByIdAndUpdate(req.user._id, {
+				name: req.body.name,
+				gender: req.body.gender,
+				phone: req.body.phone,
+			});
 
-            const user = await userModel.findById(req.user._id);
+			const user = await userModel.findById(req.user._id);
 
-            const response = {
-                user,
-                message: 'Update success',
-            };
-            return res.json(response);
-        } catch (error) {
-            if (!error.message) error.message = 'Something went wrong';
-            next(error);
-        }
-    };
+			const response = {
+				user,
+				message: "Update success",
+			};
+			return res.json(response);
+		} catch (error) {
+			if (!error.message) error.message = "Something went wrong";
+			next(error);
+		}
+	};
 
-    changePassword = async (req, res, next) => {
-        try {
-            let user = req.user;
-            const SALT_ROUNDS = 10;
-            if (!bcrypt.compareSync(req.body.oldPassword, user.password))
-                throw new CustomError(400, 'Wrong password');
+	changePassword = async (req, res, next) => {
+		try {
+			let user = req.user;
+			const SALT_ROUNDS = 10;
+			if (!bcrypt.compareSync(req.body.oldPassword, user.password))
+				throw new CustomError(400, "Wrong password");
 
-            const hashPassword = bcrypt.hashSync(
-                req.body.newPassword,
-                SALT_ROUNDS,
-            );
+			const hashPassword = bcrypt.hashSync(req.body.newPassword, SALT_ROUNDS);
 
-            user.password = hashPassword;
-            user.save();
+			user.password = hashPassword;
+			user.save();
 
-            user = await userModel.findById(req.user._id);
+			user = await userModel.findById(req.user._id);
 
-            const response = {
-                user,
-                message: 'Update success',
-            };
-            return res.json(response);
-        } catch (error) {
-            if (!error.message) error.message = 'Something went wrong';
-            next(error);
-        }
-    };
+			const response = {
+				user,
+				message: "Update success",
+			};
+			return res.json(response);
+		} catch (error) {
+			if (!error.message) error.message = "Something went wrong";
+			next(error);
+		}
+	};
 
-    getAllUser = async (req, res, next) => {
-        try {
-            const users = await userModel.find();
+	getAllUser = async (req, res, next) => {
+		try {
+			const users = await userModel.find();
 
-            const response = {
-                users,
-            };
-            return res.json(response);
-        } catch (error) {
-            if (!error.message) error.message = 'Something went wrong';
-            next(error);
-        }
-    };
+			const response = {
+				users,
+			};
+			return res.json(response);
+		} catch (error) {
+			if (!error.message) error.message = "Something went wrong";
+			next(error);
+		}
+	};
 }
 
 module.exports = new UserController();
