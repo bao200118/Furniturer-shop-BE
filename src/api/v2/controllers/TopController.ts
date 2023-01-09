aeb74f91e62cca1c502e8cd67754d5541e2a7077
bao200118 <60221952+bao200118@users.noreply.github.com>
2023-01-09 23:30:43 +0700

    Add pagination

--- src/api/v2/controllers/TopController.ts
+++ src/api/v2/controllers/TopController.ts
@@ -7,34 +7,41 @@ interface AndConditonObject {
 class TopController {
 	getTopProduct = async (req: Request, res: Response, next: NextFunction) => {
 		try {
-            const parameters = req.body;
+			const parameters = req.body;
+			const { page, pageSize } = req.params;
 			const condition = [{ $and: [] }];
 			// Determine with a group filter conditon product split by or
 			let indexOrConjunction = 0;
-            let sort = "DESC";
-            for (let index = 0; index < parameters.length; index++) {
-                const param = parameters[index];
-                if (param == null) continue;
+			let sort = "DESC";
+			for (let index = 0; index < parameters.length; index++) {
+				const param = parameters[index];
+				if (param == null) continue;
 				switch (Object.keys(param)[0]) {
 					case "product":
 						this.filterProduct(condition, indexOrConjunction, param.product);
 						break;
 					case "category":
-                        this.filterCategory(condition, indexOrConjunction, param.category);
+						this.filterCategory(condition, indexOrConjunction, param.category);
 						break;
 					case "conjunction":
-                        if(index == parameters.length - 1) break;
+						if (index == parameters.length - 1) break;
 						indexOrConjunction = this.filterConjunction(
 							condition,
 							indexOrConjunction,
 							param.conjunction
 						);
 						break;
-                        case "price":
-                            sort = this.filterPrice(param);
+					case "price":
+						sort = this.filterPrice(param);
 				}
-            }
-            const product = await productModel.find().limit(3).or(condition).sort({price: sort}).exec();
+			}
+			const product = await productModel
+				.find()
+				.limit(Number.parseInt(pageSize || "3"))
+				.skip(Number.parseInt(page || "1"))
+				.or(condition)
+				.sort({ price: sort })
+				.exec();
 			const response = {
 				product,
 			};
@@ -49,7 +56,9 @@ class TopController {
 		indexOrConjunction: number,
 		product: string
 	) => {
-		condition[indexOrConjunction].$and.push({ name: { $regex: product[0].toUpperCase() + product.substring(1) } });
+		condition[indexOrConjunction].$and.push({
+			name: { $regex: product[0].toUpperCase() + product.substring(1) },
+		});
 	};
 
 	private filterConjunction = (
@@ -72,13 +81,11 @@ class TopController {
 		condition[indexOrConjunction].$and.push({ category });
 	};
 
-    private filterPrice = (
-		price: string
-	) => {
-        if (price === "maximum") {
-            return "DESC"
-        }
-        return "ASC"
+	private filterPrice = (price: string) => {
+		if (price === "maximum") {
+			return "DESC";
+		}
+		return "ASC";
 	};
 }
 
